ae5e480283c36148c02c63f37b3ce833477b2868
VasilisSiam <105783946+VasilisSiam@users.noreply.github.com>
2023-04-27 19:17:38 +0300

    Google

---
+++ GoogleAuthAndCalendar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Last
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GoogleAuthAndCalendar/Models/Event.cs
@@ -0,0 +1,32 @@
+﻿using Google.Apis.Calendar.v3.Data;
+
+namespace Last
+{
+    public class Event
+    {
+        public Event()
+        {
+            this.Start = new EventDateTime()
+            {
+                TimeZone = "Europe/Greece"
+            };
+            this.End = new EventDateTime()
+            {
+                TimeZone = "Europe/Greece"
+            };
+        }
+
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public EventDateTime Start { get; set; }
+        public EventDateTime End { get; set; }
+
+        public class EventDateTime
+        {
+            public string DateTime { get; set; }
+            public string TimeZone { get; set; }
+
+        }
+
+    }
+}
