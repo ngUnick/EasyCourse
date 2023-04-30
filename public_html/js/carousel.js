e8bb5df34b553e452b963fee91b01a6f0033865c
Nikolaos Papadopoulos <72943192+Megamart123@users.noreply.github.com>
2023-04-30 05:07:35 +0300

    UI Update (Carousel finally + some other stuff)

--- public_html/js/carousel.js
+++ public_html/js/carousel.js
@@ -1,57 +1,7 @@
 // Set Img Attributes ----------------------------
 
-window.addEventListener('load', () => {
-    window.images = document.querySelectorAll('ul li img');
-    window.slideWidth = images[0].clientWidth;
-
-    console.log(images);
-    setImgAttributes(images, slideWidth);
-
-  });
 
 
-function setImgAttributes(images, slideWidth){
-    for (let i = 0; i < images.length; i++) {
-        const image = images[i];
-    
-        // Apply unique attributes to each image based on its position and class
-        if (i === 0) {
-    
-            image.style.height = '200px';
-            image.style.transform = 'translateX(' + (slideWidth * 1.2) + 'px)';
-            image.style.marginTop = '50px';
-            image.style.zIndex = 1;
-    
-        } else if (i === 1) {
-    
-            image.style.height = '250px';
-            image.style.transform = 'translateX(' + (slideWidth * 0.7) + 'px)';
-            image.style.marginTop = '25px';
-            image.style.zIndex = 10;
-    
-        } else if (image.classList.contains('active')) {
-
-            image.style.height = '300px';
-            image.style.transform = 'translateX(0px)';
-            image.style.zIndex = '20';
-
-        } else if (i === 3) {
-    
-            image.style.height = '250px';
-            image.style.transform = 'translateX(' + (-slideWidth * 0.7) + 'px)';
-            image.style.marginTop = '25px';
-            image.style.zIndex = 10;
-    
-        } else if (i === 4) {
-    
-            image.style.height = '200px';
-            image.style.transform = 'translateX(' + (-slideWidth * 1.2) + 'px)';
-            image.style.marginTop = '50px';
-            image.style.zIndex = 1;
-    
-        }
-        }
-}
 
 // Toggle Slider ----------------------------
 
@@ -61,6 +11,7 @@ document.addEventListener("click", (e) =>{
     }
     else if(e.target.classList.contains("slider-next")){
         toggleNext();
+        updateNext();
     }
 })
 
@@ -104,47 +55,41 @@ function toggleNext(){
     }
 }
 
+function updateNext() {
+    // Set the position of each item based on the current index
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+      let position = parseInt(item.style.order);
+      console.log(position);
+      if (position + 1 <= 4){
+        position ++;
+        item.style.order = position;
+        console.log(position);
+      } else {
+        item.style.order = 0;
+        console.log(position);
+      }
 
-function temp(images){
-    let array = Array.from(images);
-    let lastValue = array.pop(); // remove the last value from the array
-    array.unshift(lastValue);
-
-    let parentElement = images[0].parentNode; // get the parent element of the first node
-    while (parentElement.firstChild) {
-    parentElement.removeChild(parentElement.firstChild);
-    }
-    for (let i = 0; i < array.length; i++) {
-    parentElement.appendChild(array[i]);
-    }
-    let updatedNodeList = document.querySelectorAll('ul li img');
-
-    console.log(updatedNodeList);
-
-
-
-
-    const activeImg =  document.querySelector("img.active");
-    console.log(activeImg);
-
-    const activeData = parseInt(activeImg.dataset.index);
-    console.log(activeData);
-
-    if(activeData - 1 >= 10){
-        const nextImg = document.querySelector(`[data-index="${activeData-1}"]`);
-        //console.log(nextChild)
-        activeImg.classList.remove("active");
-        nextImg.classList.add("active");
-    }
-    else{
-        const nextImg = document.querySelector(`[data-index="${14}"]`);
-        //console.log(nextChild)
-        activeImg.classList.remove("active");
-        nextImg.classList.add("active");
-    }
-
-
-
-
-    setImgAttributes(updatedNodeList, slideWidth);
-}
+      
+    });
+    // Apply styles based on the position of each item
+    items.forEach((item) => {
+        let position2 = parseInt(item.style.order);
+      if (position2 == 0) {
+        item.classList.add('small1');
+        item.classList.remove('small2');
+      } else if (position2 == 1) {
+        item.classList.add('big1');
+        item.classList.remove('small1');
+      } else if (position2 == 2) {
+        item.classList.add('focus');
+        item.classList.remove('big1');
+      } else if (position2 == 3) {
+        item.classList.add('big2');
+        item.classList.remove('focus');
+      } else if (position2 == 4) {
+        item.classList.add('small2');
+        item.classList.remove('big2');
+      }
+    });
+  }
