fe453c26a3b512db4b07a6e1e8470ecece82dd05
EnaAlogo <93442086+EnaAlogo@users.noreply.github.com>
2023-05-08 20:21:45 +0300

    slider

--- public_html/js/carousel.js
+++ public_html/js/carousel.js
@@ -55,7 +55,7 @@ function setImgAttributes(images, slideWidth){
 
 // Toggle Slider ----------------------------
 
-document.addEventListener("click", (e) =>{
+document.addEventListener("click", e => {
     if(e.target.classList.contains("slider-prev")){
         togglePrevious();
     }
@@ -65,7 +65,14 @@ document.addEventListener("click", (e) =>{
 })
 
 
+<<<<<<< Updated upstream
 function togglePrevious(){
+=======
+
+// Rotate the h3 text -----------------------------------------------------------
+
+const togglePrevious = () => {
+>>>>>>> Stashed changes
     const currentActive = document.querySelector("h3.active");
     const currentIndex = parseInt(currentActive.dataset.index);
     //console.log(currentIndex);
@@ -85,7 +92,7 @@ function togglePrevious(){
 }
 
 
-function toggleNext(){
+const toggleNext = () =>{
     const currentActive = document.querySelector("h3.active");
     const currentIndex = parseInt(currentActive.dataset.index);
     //console.log(currentIndex);
@@ -119,6 +126,7 @@ function temp(images){
     }
     let updatedNodeList = document.querySelectorAll('ul li img');
 
+<<<<<<< Updated upstream
     console.log(updatedNodeList);
 
 
@@ -148,3 +156,88 @@ function temp(images){
 
     setImgAttributes(updatedNodeList, slideWidth);
 }
+=======
+const updateNext = () => {
+    // Set the position of each item based on the current index
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+      let position = parseInt(item.style.order);
+      console.log(position);
+      item.style.order = position <=3 ? position+1 : 0;
+      console.log(position);
+    });
+    // Apply styles based on the position of each item
+    items.forEach( item => PositionWiseApplyStyle(item) )
+  }
+
+const updatePrev = ()=> {
+    // Set the position of each item based on the current index
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+      let position = parseInt(item.style.order);
+      console.log(position);
+      item.style.order = position >= 1 ? position -1 : 4
+      console.log(position);
+    });
+    // Apply styles based on the position of each item
+    items.forEach( item => IndexWiseApplyStyle(item) );
+  }
+
+const PositionWiseApplyStyle = item =>
+{
+  let position2 = parseInt(item.style.order);
+        switch(position2) {
+      case 0:
+        item.classList.add('small1');
+        item.classList.remove('small2');
+        break;
+      case 1:
+        item.classList.add('big1');
+        item.classList.remove('small1');
+        break;
+      case 2:
+        item.classList.add('focus');
+        item.classList.remove('big1');
+        break;
+      case 3:
+        item.classList.add('big2');
+        item.classList.remove('focus');
+        break;
+      case 4:
+        item.classList.add('small2');
+        item.classList.remove('big2');
+        break;
+      default:
+        console.log('unexpected error')
+      }
+}
+
+const IndexWiseApplyStyle = item =>
+{
+  let position2 = parseInt(item.style.order);
+        switch(position2) {
+      case 0:
+        item.classList.add('small1');
+        item.classList.remove('big1');
+        break;
+      case 1:
+        item.classList.add('big1');
+        item.classList.remove('focus');
+        break;
+      case 2:
+        item.classList.add('focus');
+        item.classList.remove('big2');
+        break;
+      case 3:
+        item.classList.add('big2');
+        item.classList.remove('small2');
+        break;
+      case 4:
+        item.classList.add('small2');
+        item.classList.remove('small1');
+        break;
+      default:
+        console.log('unexpected error')
+      }
+}
+>>>>>>> Stashed changes
